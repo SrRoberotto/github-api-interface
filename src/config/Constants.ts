80dae79414800366b28eb4c3120e33fbe0125e14
SrRoberotto <59906851+SrRoberotto@users.noreply.github.com>
2022-07-18 23:43:37 -0300

    Inicial

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://mestre:LcrQ6Eqg95XSf4Wg@clusterprincipal.pfl5lcv.mongodb.net/?retryWrites=true&w=majority'
+}
